1f62330ab1dfe4b186d9473b09a025a04b5a7a22
johnnyli2531 <johnnyli2531@qq.com>
2022-03-16 15:48:58 +0800

    去掉有道脚本引用

--- webTranslate/dist/webTranslate.js
+++ webTranslate/dist/webTranslate.js
@@ -221,7 +221,6 @@
             url: "http://fanyi.youdao.com/",
             timeout: 5000,
             onload: function (ydRes) {
-                //fanyijsUrlMatch正则匹配字符串  <script type="text/javascript" src="http://shared.ydstatic.com/fanyi/newweb/v1.0.29/scripts/newweb/fanyi.min.js"></script>
                 var fanyijsUrlMatch = /<script\s+type="text\/javascript"\s+src="([http|https]*?:\/\/shared.ydstatic.com\/fanyi\/newweb\/v[\d.]+\/scripts\/newweb\/fanyi.min.js)"><\/script>/g.exec(ydRes.responseText);
                 if (!fanyijsUrlMatch) {
                     console.log("获取fanyi.min.js失败！！！");
@@ -233,7 +232,6 @@
                             url: fanyijsUrl,
                             timeout: 5000,
                             onload: function (r) {
-                                //sign正则匹配字符串  sign:n.md5("fanyideskweb"+e+i+"]BjuETDhU)zqSxf-=B#7m")}};
                                 var signMatch = /sign:[a-z]{1}\.md5\("fanyideskweb"\+[a-z]{1}\+[a-z]{1}\+"(.*)"\)}};/g.exec(r.responseText);
                                 if (!signMatch) {
                                     console.log("获取sign失败！！！");
